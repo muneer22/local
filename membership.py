cf109c60be4a2faadb3052a466ab4da1de0022dd
muneer22 <muneer99ashraf@gmail.com>
2017-10-27 00:53:45 +0530

    pythin on membership operator

---
+++ membership.py
@@ -0,0 +1,19 @@
+list=[1,2,3,4,5,6,7,8,9,10]
+a=2
+b=5
+c=12
+if(a in list):
+    print('a is in the list')
+else:
+    print('a is not in the list')
+
+if(b not in list):
+    print('b is not in the list')
+else:
+    print('b is in the list')
+
+if(c not in list):
+    print('c is not in the list')
+else:
+    print('c is in the list')
+
